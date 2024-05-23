e4f54458e0d2b7bf10b4f87d161745456ca850d8
Hexcha <tanerayranci@yandex.com>
2024-05-23 21:03:17 +0300

    Border update
    Before the update, when you touch the border of the screen; game would end. But now, you are continuing from the opposite side. Only way to die is crashing into your body

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -85,7 +85,7 @@ public class GamePanel extends JPanel implements ActionListener {
     }
     public void  draw(Graphics g){
         if(running){
-            /* I used this because I wanted to see the pixels I'm working with
+            /*I used this because I wanted to see the pixels I'm working with
 
             for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
                 g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
@@ -93,17 +93,20 @@ public class GamePanel extends JPanel implements ActionListener {
             }
             */
 
+            //drawing the apple
             g.setColor(Color.red);
             g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
 
+            //drawing the snake
             for (int i = 0; i <bodyParts ; i++) {
-
+                //the head
                 if(i==0){
 
                     g.setColor(Color.green);
                     g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
 
                 }
+                //the body
                 else {
 
                     g.setColor(new Color(45,180,0));
@@ -131,51 +134,63 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void createApple() {
-
-        boolean appleOnSnake = true;
-        while (appleOnSnake) {
-
+        boolean appleOnSnake;
+        do {
+            appleOnSnake = false;
             appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
             appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
-
-            // Check if the apple position overlaps with any part of the snake's body
             for (int i = 0; i < bodyParts; i++) {
-
                 if (appleX == x[i] && appleY == y[i]) {
                     appleOnSnake = true;
                     break;
-                } else {
-                    appleOnSnake = false;
                 }
-
             }
+        } while (appleOnSnake);
+    }
+
+
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+
+        //First tried in the CheckCollisions() method but
+        //it wasn't updating fast enough. Sometimes it
+        //allowed the head to pass through body.
+        if (x[0] < 0) {
+            x[0] = SCREEN_WIDTH - UNIT_SIZE;
+            checkCollisions();
+        } else if (x[0] >= SCREEN_WIDTH) {
+            x[0] = 0;
+            checkCollisions();
+        }
+
+        if (y[0] < 0) {
+            y[0] = SCREEN_HEIGHT - UNIT_SIZE;
+            checkCollisions();
+        } else if (y[0] >= SCREEN_HEIGHT) {
+            y[0] = 0;
+            checkCollisions();
         }
     }
-    public void move(){
 
-        for (int i = bodyParts; i>0 ; i--) {
 
-            x[i]=x[i-1];
-            y[i]=y[i-1];
-
-        }
-        switch (direction){
-            case'U':
-                y[0]=y[0]-UNIT_SIZE;
-                break;
-            case'D':
-                y[0]=y[0]+UNIT_SIZE;
-                break;
-            case'L':
-                x[0]=x[0]-UNIT_SIZE;
-                break;
-            case'R':
-                x[0]=x[0]+UNIT_SIZE;
-                break;
-
-        }
-
-    }
     public void checkApple(){
 
         if((x[0]==appleX)&& (y[0]==appleY)){
@@ -186,19 +201,20 @@ public class GamePanel extends JPanel implements ActionListener {
         }
 
     }
-    public void checkCollisions(){
+    public void checkCollisions() {
 
-        for (int i = bodyParts; i>0 ; i--) {
-            if((x[0]== x[i])&& (y[0]==y[i]))
-                running=false;
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
         }
 
-        if(x[0]<0 || x[0]>SCREEN_WIDTH-(UNIT_SIZE) || y[0]<0 || y[0]>SCREEN_HEIGHT-UNIT_SIZE)
-            running=false;
-        if(!running)
+        if (!running) {
             timer.stop();
-
+        }
     }
+
+
     public void gameOver(Graphics g){
 
 
